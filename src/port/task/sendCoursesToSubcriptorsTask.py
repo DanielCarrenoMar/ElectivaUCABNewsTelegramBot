262b1a760d5b05184a830d80c1d9d0905a3c7b3c
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2026-08-17 20:59:33 -0400

    fix: import

--- src/port/task/sendCoursesToSubcriptorsTask.py
+++ src/port/task/sendCoursesToSubcriptorsTask.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 from telebot import TeleBot
 
-from aplication.sendCourseToSubcriptorsUseCase import SendCourseToSubcriptorsUseCase
+from src.aplication.sendCourseToSubcriptorsUseCase import SendCourseToSubcriptorsUseCase
 from src.infraestructure.repositoryImp.telegramNotifierRepositoryImp import TelegramNotifierRepositoryImp
 
 
