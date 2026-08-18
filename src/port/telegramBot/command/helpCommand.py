637d42f3271d4198688c9d1cfc4e47cb03db38f6
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2026-08-18 16:09:35 -0400

    feat: colcoar comando de help a español "ayuda"

--- src/port/telegramBot/command/helpCommand.py
+++ src/port/telegramBot/command/helpCommand.py
@@ -1,12 +1,12 @@
 def register(bot):
-    @bot.message_handler(commands=["help"])
+    @bot.message_handler(commands=["ayuda"])
     def handle_help(message):
         bot.reply_to(
             message,
             (
                 "<b>Comandos</b>\n"
                 "/start - iniciar bot en este chat\n"
-                "/help - mostrar esta ayuda\n"
+                "/ayuda - mostrar esta ayuda\n"
                 "/suscribirse - activar notificaciones de cursos\n"
                 "/desuscribirse - pausar notificaciones de cursos\n"
                 "/filtros - ver y cambiar los filtros de búsqueda"
